161341d216e720bbb769f98eb60fcc64100597ee
owaiss22 <owaisiqbalhussain@gmail.com>
2020-02-18 18:22:10 +0500

    javascript added project complete

---
+++ public/store.js
@@ -0,0 +1,146 @@
+/** jb hum async krdete h script tou body background m load hoti h */
+/** aur ye ready ka func make sure kr rha hay k k jb body load hojaye phr script run karo */
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+    var removebtn = document.getElementsByClassName("btn-danger")
+    for (var i = 0; i < removebtn.length; i++) {
+        var button = removebtn[i]
+        button.addEventListener('click', removeCartItem)
+        // button.addEventListener('click', function (event) {
+        //     var buttonClicked = event.target
+        //     buttonClicked.parentElement.parentElement.remove();
+        //     updateCartTotal()
+        // })
+    }
+
+    var quantityInputs = document.getElementsByClassName("cart-quantity-input")
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+
+    var addToCartButtons = document.getElementsByClassName("shop-item-button")
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+}
+
+function purchaseClicked() {
+    alert('Thank you for your purchase')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+}
+
+/** alag func m daala code easily read ho aur smjh aye */
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal()
+}
+
+
+/** pehle aese kia tha  */
+
+// var removebtn = document.getElementsByClassName("btn-danger")
+// //  console.log(removebtn)
+// for (var i = 0; i < removebtn.length; i++) {
+//     // console.log(removebtn[i])
+//     var button = removebtn[i]
+//     button.addEventListener('click', function(event){
+//         // console.log("clicked")
+//         var buttonClicked = event.target
+//         buttonClicked.parentElement.parentElement.remove();
+//         updateCartTotal()
+//     })
+// }
+
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    // console.log(shopItem)
+    var title = shopItem.getElementsByClassName("shop-item-title")[0].innerText
+    var price = shopItem.getElementsByClassName("shop-item-price")[0].innerText
+    var imageSrc = shopItem.getElementsByClassName("shop-item-image")[0].src
+    // console.log(title, price, imageSrc)
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
+
+// function addItemToCart(title, price, imageSrc) {
+//     var cartRow = document.createElement("div")
+//     cartRow.innerText = title
+//     var cartItems = document.getElementsByClassName("cart-items")[0]
+//     cartItems.append(cartRow)
+// }
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            alert('This item is already added to the cart')
+            return
+        }
+    }
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>
+        `
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    /** ye jo bd m cart m item add hue h un ka remove btn kaam nhi krta q k humne ye kaam doc m load hone pr kia tha */
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+}
+
+
+
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName("cart-items")[0]
+    var cartRows = cartItemContainer.getElementsByClassName("cart-row")
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName("cart-price")[0]
+        var quantityElement = cartRow.getElementsByClassName("cart-quantity-input")[0]
+        // console.log(priceElement, quantityElement)
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value
+        // console.log(price * quantity)
+        total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName("cart-total-price")[0].innerText = '$' + total
+}
+
+
